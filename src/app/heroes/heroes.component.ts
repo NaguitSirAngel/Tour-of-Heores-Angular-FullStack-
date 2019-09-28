9fbfdb2fb89d6bc707de1d9ed120fae000df69ba
Sir Angel Naguit <angelnaguit@icloud.com>
2019-09-28 16:26:40 -0400

    'added css to heroes.component.ts'

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../Hero'
+import{ HEROES } from '../mock-heroes'; 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
- //A Class instantiation
+ 
+  //A Class instantiation 
+  selectedHero: Hero;
   
- hero: Hero = {
-    id :  1,
-    name: "Windstorm"
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
+
+
+  
+  heroes = HEROES;
 
   constructor() { }
 
