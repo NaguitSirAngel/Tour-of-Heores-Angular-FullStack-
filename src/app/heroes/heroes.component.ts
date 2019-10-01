5d0f5a1ab8e722bf40a02b8c35dae4866d4ca76c
Sir Angel Naguit <angelnaguit@icloud.com>
2019-10-01 17:01:26 -0400

    Angular Part4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../Hero'
 import{ HEROES } from '../mock-heroes'; 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,18 +12,31 @@ export class HeroesComponent implements OnInit {
  
   //A Class instantiation 
   selectedHero: Hero;
-  heroes = HEROES;
+  heroes: Hero[];
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+        /** 
+         * getHeroesSubscribe(heroes){
+         * this.heroes = heroes;
+         * }
+         * 
+        */
+  }
+
 
  
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+
+    this.getHeroes();
     
   }
 
